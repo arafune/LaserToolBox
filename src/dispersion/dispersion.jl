71de17a04e4b4f38dc571f98eea2c6e030b81804
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-01-19 19:31:41 +0900

    🎨  Use beta_n (but further test is required...)

--- src/dispersion/dispersion.jl
+++ src/dispersion/dispersion.jl
@@ -4,6 +4,8 @@ include("materials/materials.jl")
 include("dispersive_optics/dispersive_optics.jl")
 include("orders.jl")
 
+export beta_n, gvd, tod
+
 """Group Velocity Dispersion (GVD) in [fs²/μm]"""
 const gvd(model, λ) = beta_n(model, λ, 2)
 
@@ -17,8 +19,6 @@ using .DispersiveOptics: PrismPair, gdd, brewster_angle_deg, ideal_apex_deg
 
 export DispersiveOptics
 export PrismPair, gdd, brewster_angle_deg, ideal_apex_deg
-
-export beta_n, gvd, tod
 
 import ..RefractiveIndex
 const n = RefractiveIndex
